e573589ec3d839f8840b011cf7e1cfcc5af18f44
gusdk45 <gusdk1418@naver.com>
2023-01-31 04:39:39 +0900

    footer, mypage

---
+++ app/src/main/java/com/example/mypet/MyPage.kt
@@ -0,0 +1,12 @@
+package com.example.mypet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mypage)
+
+    }
+}
